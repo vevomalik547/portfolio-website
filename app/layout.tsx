7bc9f2ad86188b0ad841eb6defaaa908033ec267
vevomalik547 <vevomalik547@gmail.com>
2024-11-02 21:48:57 +0530

    Updated Name tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Abdul Malik",
+  title: "Abdul Malik | Portfolio",
   description: "Abdul Malik is a full-stack developer with 3 years of experience.",
 };
 
